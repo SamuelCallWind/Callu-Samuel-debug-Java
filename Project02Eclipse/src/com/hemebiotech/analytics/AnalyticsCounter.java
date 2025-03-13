d6b7905873bc3fd180ff4edc188a6fe107d7789c
Sam_callwind <samcallwind@gmail.com>
2025-03-13 18:55:46 +0100

    Created a new class to handle the symptoms

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,42 @@
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
+
 import java.io.FileReader;
 import java.io.FileWriter;
 
-public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
+
+
+/*
+ * Implementation of a  AnalyticsCounter that is reading symptoms from the symptom file.
+ *  This class is reading each line of the document and writes them back in the result.out file
+ * For that, another class Symptom is created where both static methods are used
+ */
+
+
+public class AnalyticsCounter {	// initialize to 0
+
 	public static void main(String args[]) throws Exception {
-		// first get input
+		/*
+		 * Creating the reader to open and read the symptom file and scanning each line
+		 */
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
-
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
 		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
+			
+			Symptom.addSymptom(line);
+			line = reader.readLine();	// get another symptom (or read another line)
 		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+		Symptom.writeTotalSymptoms();
 	}
 }
+
+
+
+
+// counts headaches
+
+// Deleted the useless i incrementation as it was not used for anything.
+//Changed the incrementation of headacheCount instead of the headache variable 
+
+//There was a typo at the "rash". It was written "rush" instead. So that is why the result were not printed correctly
